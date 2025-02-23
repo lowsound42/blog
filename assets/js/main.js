82c1d4715952e4427822693de45fd0495448276a
Omar Khan <o.khandxb@gmail.com>
2025-02-23 15:41:35 +0530

    some work done on the desert

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+let hiddenElement = document.getElementsByClassName("hidden");
+let loadingElement = document.getElementsByClassName("loader");
+
+function clearLoader() {
+  if (loadingElement[0] !== undefined)
+    loadingElement[0].classList.add("hidden");
+  if (hiddenElement[0] !== undefined)
+    hiddenElement[0].classList.remove("hidden");
+}
+
+const loaderTimeout = setTimeout(clearLoader, 2000);
