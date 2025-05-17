a7f81bc8ecc0821e23eb7953c27a165aabe804bc
Omar Khan <o.khandxb@gmail.com>
2025-05-17 13:08:18 -0400

    photo section added

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+import * as Fancybox from "./fancy-box.js";
+
+// Set custom options for elements with the `data-fancybox="gallery"` attribute
+Fancybox.Fancybox.bind('[data-fancybox="gallery"]', {
+  groupAll: true,
+});
