d92b241092b012cbbf71b9f96a4b04f88d6caea1
lowsound42 <o.khandxb@gmail.com>
2025-12-07 18:39:47 -0500

    testing deployment

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 import * as Fancybox from "./fancy-box.js";
-
+// testing deploy
 // Set custom options for elements with the `data-fancybox="gallery"` attribute
 Fancybox.Fancybox.bind('[data-fancybox="gallery"]', {
-  groupAll: true,
+    groupAll: true,
 });
